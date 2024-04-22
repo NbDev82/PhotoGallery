394cc11900a4edc09679d474e85a90130d6b8916
Van Hoang <21110828@student.hcmute.edu.vn>
2024-04-23 01:25:14 +0700

    add thumbnail in view image

--- app/src/main/java/com/example/photogallery/PhotoActivity.java
+++ app/src/main/java/com/example/photogallery/PhotoActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.Priority;
 import com.example.photogallery.databinding.ActivityImageBinding;
 
 public class PhotoActivity extends AppCompatActivity {
@@ -24,7 +25,13 @@ public class PhotoActivity extends AppCompatActivity {
         Uri uri = getIntent().getData();
 
         if (uri != null) {
-            Glide.with(binding.getRoot()).load(uri).into(binding.imageView);
+            Glide.with(binding.getRoot())
+                    .load(uri)
+                    .error(R.drawable.ic_error)
+                    .placeholder(R.drawable.ic_loading)
+                    .thumbnail(0.02f)
+                    .priority(Priority.IMMEDIATE)
+                    .into(binding.imageView);
         } else {
             Toast.makeText(this, "Image is unavailable!", Toast.LENGTH_SHORT).show();
         }
